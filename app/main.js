ffea331cf7469c6c7324d11915bc58ba913acb64
N4NO1 <nathan.rouse56734@gmail.com>
2022-04-01 12:19:08 +0100

    Added comments

--- app/main.js
+++ app/main.js
@@ -5,51 +5,60 @@ const csvWriteStream = require('csv-write-stream')
 const csvParser = require("csv-parser")
 var csvWriter = csvWriteStream()
 
-
+//assigning the file path from CLI Arg 2, 
+//can be set by calling method by changing variable in readCsv function call
 const filePath = process.argv[2]
 
-function readCsv(filePath) {
-const stream = fs.createReadStream(filePath)
-    .pipe(csv({ mapHeaders: ({ header, index }) => header.toLowerCase().trim() }))
-    .on("headers", (headers) => {
-        stream.pause()
-        var exists = checkHeaders(headers)
-        if ( exists === false) {
-            console.error("header 'name' is missing but required")
-            stream.end()
-        }
-        else {
-            headers.splice(headers.indexOf("prefix"),1)
-            headers.splice(headers.indexOf("price"),1)
-            headers.splice(headers.indexOf("splitter"),1)
-            headers.splice(headers.indexOf("name"),1)
-            headers = headers.map(n => "attribute " + n)
-            newHeaders = ["sku","name","salePrice", "taxRate", "is variable"].concat(headers)
-            csvWriter.headers = newHeaders
-            csvWriter.pipe(fs.createWriteStream("output.csv"))
-            stream.resume()
-        }
-    })
-    .on("data", (data) => {
-        stream.pause()
+readCsv(filePath)
 
-        control(data).then(() => { stream.resume() })
-            .catch((e) => {
-                console.error("error",e)
+//takes a filepath and creates the readStream for the CSV file
+function readCsv(filePath) {
+
+    //create the stream using the filePath parameter
+    const stream = fs.createReadStream(filePath)
+    //map headers and convert them to lower case
+        .pipe(csv({ mapHeaders: ({ header, index }) => header.toLowerCase().trim() }))
+        //on the header row, check that certain columns exist, and create headers for write stream
+        .on("headers", (headers) => {
+            stream.pause()
+            var exists = checkHeaders(headers)
+            //if columns don't exist, error with a message, and end the stream
+            if (exists === false) {
+                console.error("header 'name' is missing but required")
                 stream.end()
-            })
-    })
-    .on("end", () => {
-        console.log("Done")
-        csvWriter.end()
-    })
+            }
+            else {
+                headers.splice(headers.indexOf("prefix"), 1)
+                headers.splice(headers.indexOf("price"), 1)
+                headers.splice(headers.indexOf("splitter"), 1)
+                headers.splice(headers.indexOf("name"), 1)
+                headers = headers.map(n => "attribute " + n)
+                newHeaders = ["sku", "name", "salePrice", "taxRate", "is variable"].concat(headers)
+                csvWriter.headers = newHeaders
+                csvWriter.pipe(fs.createWriteStream("output.csv"))
+                stream.resume()
+            }
+        })
+        .on("data", (data) => {
+            stream.pause()
+
+            control(data).then(() => { stream.resume() })
+                .catch((e) => {
+                    console.error("error", e)
+                    stream.end()
+                })
+        })
+        .on("end", () => {
+            console.log("Done")
+            csvWriter.end()
+        })
 }
 
 function checkHeaders(headers = []) {
     var requiredHeadersExist = 0
 
     requiredHeadersExist += (headers.includes("name") === true ? 0 : 1)
-    
+
     if (requiredHeadersExist == 0) {
         return true
     }
@@ -70,8 +79,8 @@ function control(column) {
 
     var end = new Date().getTime()
 
-    console.log(`Created `, `Time: ${end-start}ms`)
-    
+    console.log(`Created `, `Time: ${end - start}ms`)
+
     const written = writeVariants(variations)
 
 
@@ -84,7 +93,7 @@ function createObject(columnData) {
         name: columnData.name,
         prefix: columnData.prefix ?? "",
         splitter: columnData.splitter ?? "",
-        salePrice:columnData.price ?? 0,
+        salePrice: columnData.price ?? 0,
         taxRate: columnData.taxRate ?? "zero rated",
         attributes: [],
         possibilities: 1
@@ -115,23 +124,23 @@ function createObject(columnData) {
 }
 
 function generateVariants(variableObject = {}) {
-    var variantArray=[]
+    var variantArray = []
 
     var lastStepCount = variableObject.possibilities
 
     //intialise the array with the variants starting details
     for (let i = 0; i < variableObject.possibilities; i++) {
         variantArray[i] = {
-            sku:variableObject.prefix,
-            name:variableObject.name,
-            salePrice:variableObject.salePrice,
-            taxRate:variableObject.taxRate
+            sku: variableObject.prefix,
+            name: variableObject.name,
+            salePrice: variableObject.salePrice,
+            taxRate: variableObject.taxRate
         }
     }
 
     variableObject.attributes.forEach(index => {
         lastStepCount /= index.values.length
-        variantArray = handleAttribute(index, lastStepCount, variableObject, variantArray, (lastStepCount === 1 ? "" :variableObject.splitter))
+        variantArray = handleAttribute(index, lastStepCount, variableObject, variantArray, (lastStepCount === 1 ? "" : variableObject.splitter))
     })
     return variantArray
 }
@@ -146,7 +155,7 @@ function handleAttribute(attributeObject, perStep, variableObject, variants, spl
         for (let j = 0; j < perStep; j++) {
             variants[i].sku = variants[i].sku + attributeObject.values[value] + split
             variants[i].name = variants[i].name + attributeObject.values[value] + split
-            variants[i]["attribute " + attributeObject.name] =  attributeObject.values[value]
+            variants[i]["attribute " + attributeObject.name] = attributeObject.values[value]
 
             i++
         }
@@ -156,7 +165,7 @@ function handleAttribute(attributeObject, perStep, variableObject, variants, spl
 }
 
 function writeVariants(variants = []) {
-    variants.forEach(variant =>{
+    variants.forEach(variant => {
         csvWriter.write(variant)
     })
     return true
